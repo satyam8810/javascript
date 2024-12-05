fba336abe2bc34d7086fb96a8d9e49d31305c4ca
satyam8810 <satyamtripathi1606@gmail.com>
2024-12-05 16:37:05 +0530

    Add files via upload

---
+++ Data Type.js
@@ -0,0 +1,25 @@
+var a=75;
+var b=500;
+
+var sum=a+b;
+console.log("Sum value:           ",sum);
+
+var sub=a-b;
+console.log("Substraction:        ",sub);
+
+var multi=a*b;
+console.log("Multiplication:      ",multi);
+
+var divide=a/b;
+console.log("Divide:              ",divide);
+
+var power=a**3;
+console.log("Power:               ",power);
+
+var modules=40%a;
+console.log("Reminder:            ",modules);
+
+
+
+
+
